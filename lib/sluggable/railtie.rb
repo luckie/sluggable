84c8ba59814b409e2627935380cf34ee41f97819
Tom Miller <tom.miller@luckie.com>
2012-01-09 10:10:47 -0600

    Add Validations to ActiveModel for reusability

--- lib/sluggable/railtie.rb
+++ lib/sluggable/railtie.rb
@@ -1,7 +1,7 @@
 module Sluggable
   class Railtie < Rails::Railtie
     initializer 'sluggable.configure_rails_initialization' do
-      ActiveRecord::Validations.send :include, Sluggable::Validations
+      ActiveModel::Validations.send :include, Sluggable::Validations
       ActiveRecord::Base.send :include, Sluggable::Sluggify
     end
   end
